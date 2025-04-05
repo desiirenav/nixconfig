73ec315d0d81b7c9aa3b813ff4bbf65b680f7a24
desiirenav <desiirenav@hotmail.com>
2025-04-05 02:17:39 -0400

    pipewire

--- hosts/default/config.nix
+++ hosts/default/config.nix
@@ -47,6 +47,15 @@
     LC_TIME = "en_CA.UTF-8";
   };
 
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+  };
+
   # User
   users.users.narayan = {
     isNormalUser = true;
